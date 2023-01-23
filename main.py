ffe5bb999a9ce726f047e59c3e1b73e920b01944
Anton Dale <anton.mail@zoho.com>
2023-01-23 01:09:03 -0500

    Add screensaver, 'if __name__ == '__main__'' and docstrings

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from random import randint
 
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Атакуй.
+
+    Генерирует количество очков атаки в зависимости от
+    выбранного типа персонажа и возвращает строковое
+    сообщение о проведённой атаке.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +26,12 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Блокируй атаку.
+
+    Генерирует количество очков защиты в зависимости от
+    выбранного типа персонажа и возвращает строковое сообщение
+    о выполненном блокировании атаки.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +42,11 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Примени специальное умение.
+
+    В зависимости от выбранного типа персонажа
+    возвращает сообщение о применении специального умения.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -38,6 +60,11 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Запусти цикл тренировки навыков персонажа.
+
+    В качестве параметров она получает введённое
+    игроком имя персонажа и выбранный тип персонажа.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -63,6 +90,11 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбери персонажа.
+
+    Позволяет игроку выбрать тип игрового персонажа
+       и возвращает выбранный вариант.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -84,7 +116,13 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    """Главная функция.
+
+    Она запускает игру,
+    и из неё вызываются все вспомогательные функции.
+    """
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -94,6 +132,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
